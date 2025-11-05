44cf7912e5e3d6f04032f68177f074816b737436
João Vitor Borges <147542970+joaoviborges@users.noreply.github.com>
2025-11-05 19:19:10 -0300

    Enhance TR_Auditoria_Viagem trigger functionality
    Added handling for UPDATE and DELETE operations in the trigger.

--- Segurança/Auditoria/trigger_Auditoria_Viagem.sql
+++ Segurança/Auditoria/trigger_Auditoria_Viagem.sql
@@ -12,3 +12,29 @@ BEGIN
         (SELECT * FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
     FROM inserted i;
 END
+
+-- UPDATE
+IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
+BEGIN
+    INSERT INTO Auditoria (TabelaAfetada, Operacao, ChavePrimaria, DadosAntigos, DadosNovos)
+    SELECT
+        'Viagem',
+        'UPDATE',
+        CAST(i.IdViagem AS VARCHAR(100)),
+        (SELECT * FROM deleted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER),
+        (SELECT * FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
+    FROM inserted i;
+END
+
+-- DELETE
+IF EXISTS (SELECT * FROM deleted) AND NOT EXISTS (SELECT * FROM inserted)
+BEGIN
+    INSERT INTO Auditoria (TabelaAfetada, Operacao, ChavePrimaria, DadosAntigos)
+    SELECT
+        'Viagem',
+        'DELETE',
+        CAST(d.IdViagem AS VARCHAR(100)),
+        (SELECT * FROM deleted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
+    FROM deleted d;
+END
+END;
