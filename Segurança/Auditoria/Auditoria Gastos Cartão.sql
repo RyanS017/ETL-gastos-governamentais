3d9317e01de4958906444f7e23d780e9566518bc
João Vitor Borges <147542970+joaoviborges@users.noreply.github.com>
2025-11-12 18:26:41 -0300

    Refactor trigger for GastosCartao auditing

--- Segurança/Auditoria/Auditoria Gastos Cartão.sql
+++ Segurança/Auditoria/Auditoria Gastos Cartão.sql
@@ -12,3 +12,30 @@ BEGIN
         (SELECT * FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
     FROM inserted i;
 END
+
+-- UPDATE
+IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
+BEGIN
+    INSERT INTO Auditoria (TabelaAfetada, Operacao, ChavePrimaria, DadosAntigos, DadosNovos)
+    SELECT
+        'GastosCartao',
+        'UPDATE',
+        CAST(i.IdGastosCartao AS VARCHAR(100)),
+        (SELECT * FROM deleted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER),
+        (SELECT * FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
+    FROM inserted i;
+END
+
+-- DELETE
+IF EXISTS (SELECT * FROM deleted) AND NOT EXISTS (SELECT * FROM inserted)
+BEGIN
+    INSERT INTO Auditoria (TabelaAfetada, Operacao, ChavePrimaria, DadosAntigos)
+    SELECT
+        'GastosCartao',
+        'DELETE',
+        CAST(d.IdGastosCartao AS VARCHAR(100)),
+        (SELECT * FROM deleted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER)
+    FROM deleted d;
+END
+END;
+
